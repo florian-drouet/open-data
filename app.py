c00ea9e4928a648d627dda42f96f4a68a1f4c1ab
FlorianDrouet46 <florian.drouet78@gmail.com>
2021-03-29 23:16:38 +0200

    Clean class geopandas_dataviz
    Make functions to prepare data
    Clean and comment dataviz_class

--- app.py
+++ app.py
@@ -1,24 +1,11 @@
 import pandas as pd
-from func.geopandas import geopandas_dataviz
+from func.geopandas import geopandas_dataviz, prepare_data_hospit, prepare_data_covidom
 import datetime
 
 if __name__ == '__main__':
 
     study_day = datetime.date(2021,2,26)
+    data = prepare_data_hospit(study_day)
 
-    df = pd.read_csv("data/donnees-hospitalieres-covid19-2021-03-29-18h03.csv", sep=";", parse_dates=['jour'])
-    df['jour_date'] = df.jour.dt.date
-    df_groupby = df.groupby(by=['dep', 'jour_date']).sum().reset_index()
-    df_day = df_groupby.loc[df_groupby.jour_date==study_day]
-    df_day = df_day.set_index('dep')
-
-    sidep = pd.read_csv("data/sp-pe-tb-quot-dep-2021-03-29-18h20.csv", sep=";", parse_dates=['jour'], low_memory=False)
-    sidep['jour_date'] = sidep.jour.dt.date
-    sidep_day = sidep.loc[sidep.jour_date==study_day].groupby(by=['dep']).sum()
-
-    merged_data = pd.merge(df_day, sidep_day, left_index=True, right_index=True).reset_index()
-    merged_data['hospit_relative'] = merged_data.hosp.div(merged_data["pop"])*1000
-
-    #df = pd.read_csv('data/donnees-hospitalieres-nouveaux-covid19-2021-03-28-19h03.csv', sep=';')
     viz = geopandas_dataviz(size=10, title=f'Hospitalisation pour 1000 habitants par département le {study_day.strftime("%d-%m-%Y")}')
-    viz.plot(dataframe=merged_data, groupby_column='dep', agg_func='sum', plot_column='hospit_relative', cmap='plasma', shrink=0.65)
+    viz.plot(dataframe=data, plot_column='hospit_relative', cmap='plasma', shrink=0.65)
--- func/geopandas.py
+++ func/geopandas.py
@@ -3,7 +3,19 @@ import pandas as pd
 from matplotlib import pyplot as plt
 
 class geopandas_dataviz():
-    
+    '''
+    Class used to visualize data per french department.
+    Input :
+        - dataframe : a dataframe indexed on french departments (type : str)
+        - plot_column : the name of the column containing the data to plot (type : str)
+        - size : the size of the graph (type : int)
+        - title : the title to display (type : str)
+        - (Optional) shrink to adapt the size of the legend box (type : float)
+        - (Optional) idf, if set to True then displays only Ile-de-France region (type : boolean)
+    Output :
+        - matplotlib graph
+    '''
+
     def __init__(self, size, title):
         self.geojson_file = geopandas.read_file('data/departements.json').set_index('code')
         self.liste_dpt_idf = ["75", "77", "78", "91", "92", "93", "94", "95"]
@@ -14,19 +26,18 @@ class geopandas_dataviz():
         self.fig = None
         self.ax = None
     
-    def preprocess(self, dataframe, groupby_column, agg_func, idf):
+    def preprocess(self, dataframe, idf):
         if idf == True:
-            self.data = dataframe.groupby(by=groupby_column).agg(agg_func).reindex(self.geojson_idf.index)
+            self.data = dataframe.reindex(self.geojson_idf.index)
         else:
-            self.data = dataframe.groupby(by=groupby_column).agg(agg_func).reindex(self.geojson_file.index)
+            self.data = dataframe.reindex(self.geojson_file.index)
     
     def define_figure(self):
         self.fig = plt.figure(figsize=(self.size, self.size))
         self.ax = plt.gca()
         self.ax.set_title(self.title)
     
-    def show_map(self, plot_column, shrink, idf, **kwargs):
-    
+    def show_map(self, plot_column, shrink, idf, **kwargs):    
         if self.ax is None:
             self.ax = plt.gca()    
 
@@ -40,7 +51,46 @@ class geopandas_dataviz():
         self.ax.set_facecolor('white')
         plt.show()
         
-    def plot(self, dataframe, groupby_column, agg_func, plot_column, shrink, idf=False, **kwargs):
-        self.preprocess(dataframe, groupby_column, agg_func, idf)
+    def plot(self, dataframe, plot_column, shrink=0.7, idf=False, **kwargs):
+        self.preprocess(dataframe, idf)
         self.define_figure()
         self.show_map(plot_column, shrink, idf, **kwargs)
+
+def prepare_data_hospit(study_day):
+    '''
+    Prepare data for geopandas data visualization.
+    Input :
+        -d dataframe donnees_hospitalieres (source : Santé Publique France)
+        - dataframe population par departement (source : SIDEP)
+    Output :
+        - Preprocessed dataframe to inject in geopandas_dataviz class
+    '''
+    # read SPF data
+    dataframe = pd.read_csv("data/donnees-hospitalieres-covid19-2021-03-29-18h03.csv", sep=";", parse_dates=['jour'])
+    # converts from datetime to date format
+    dataframe['jour_date'] = dataframe.jour.dt.date
+    # group by department and date and aggregate data as a sum
+    dataframe_groupby = dataframe.groupby(by=['dep', 'jour_date']).sum().reset_index()
+    # filter on study day
+    dataframe_day = dataframe_groupby.loc[dataframe_groupby.jour_date==study_day]
+    # set department as dataframe_day's index
+    dataframe_day = dataframe_day.set_index('dep')
+
+    # read SIDEP data
+    sidep = pd.read_csv("data/sp-pe-tb-quot-dep-2021-03-29-18h20.csv", sep=";", parse_dates=['jour'], low_memory=False)
+    # converts from datetime to date
+    sidep['jour_date'] = sidep.jour.dt.date
+    # filter on last available data, groupby and aggregate by sum
+    print(sidep.jour_date.iloc[-1])
+    sidep_day = sidep.loc[sidep.jour_date==sidep.jour_date.iloc[-1]].groupby(by=['dep']).sum()
+
+    # merging SPF and SIDEP dataframe
+    merged_data = pd.merge(dataframe_day, sidep_day, left_index=True, right_index=True).reset_index()
+    # number of hospit and rea covid for a 1000 inhabitants
+    print(merged_data.columns)
+    merged_data['hospit_relative'] = merged_data.hosp.div(merged_data["pop"])*1000
+    merged_data['rea_relative'] = merged_data.rea.div(merged_data["pop"])*1000
+    return merged_data
+
+def prepare_data_covidom(dataframe, groupby_column, agg_func):
+    return dataframe.groupby(by=groupby_column).agg(agg_func)
